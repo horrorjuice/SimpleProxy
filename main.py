3a5f1962502626b19c2312a4fe1801efbfbfb5b0
horrorjuice <ale.baturin@gmail.com>
2022-03-14 00:51:08 +1000

    1

--- main.py
+++ main.py
@@ -9,9 +9,13 @@ SITE_NAME = 'https://news.ycombinator.com/'
 @app.route('/<path:path>')
 def proxy(path):
   resp = get(f'{SITE_NAME}{path}').text
+  resp = re.sub('''\b\w{6}(?![><"=:\/'#-])\b''', '!', resp)
 
   return Response(resp)
 
 
 if __name__ == '__main__':
-  app.run(host='127.0.0.1', port=8000)
+  app.run(host='127.0.0.1', port=8001)
+
+
+
